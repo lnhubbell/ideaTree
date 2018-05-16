36214f0e436b8128b877ffead396d22d0ccaac86
Lawrence Hubbell <nathan@pellego.com>
2018-05-16 15:28:29 -0700

    move routes to a constants folder

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -5,33 +5,25 @@ import {Route, NavLink, Switch} from 'react-router-dom';
 import Ideas from '../Ideas/Ideas';
 import About from '../../components/About/About';
 import Home from '../../components/Home/Home';
+import * as routes from '../../constants/routes';
 
 import './Layout.css';
 
+// TODO: turn back to funcitonal
 class Layout extends Component {
-    URL = {
-        ideas: "/ideas",
-        themes: "/themes",
-        about: "/about",
-        home: "/",
-    }
-
-
     render() {
         return (
             <div>
-                <nav className="NavBar">
-                    <NavLink exact className="NavElem" to={this.URL.home}>Home</NavLink>
-                    <NavLink className="NavElem" to={this.URL.ideas}>Ideas</NavLink>
-                    {/* <NavLink className="NavElem" to={this.URL.themes}>Themes</NavLink> */}
-                    <NavLink className="NavElem" to={this.URL.about}>About</NavLink>
-                </nav>
-                <main>
-                    <Switch>
-                        <Route exact path={this.URL.ideas} exact component={Ideas}/>
-                        {/* <Route exact path={this.URL.themes} exact component={Themes}/> */}
-                        <Route exact path={this.URL.about} exact component={About}/>
-                        <Route exact path={this.URL.home} exact component={Home}/>
+              <nav className="NavBar">
+                <NavLink exact className="NavElem" to={routes.LANDING}>Home</NavLink>
+                <NavLink className="NavElem" to={routes.IDEAS}>Ideas</NavLink>
+                <NavLink className="NavElem" to={routes.ABOUT}>About</NavLink>
+              </nav>
+              <main>
+                <Switch>
+                  <Route exact path={routes.IDEAS} exact component={Ideas}/>
+                  <Route exact path={routes.ABOUT} exact component={About}/>
+                  <Route exact path={routes.LANDING} exact component={Home}/>
                     </Switch>
                 </main>
             </div>
